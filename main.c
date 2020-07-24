e2f37c3c063e07f7ba34f1659f3fe47393cca452
Noah Swerhun <nswerhun@gmail.com>
2020-07-24 02:03:58 -0500

    added color output

--- main.c
+++ main.c
@@ -22,31 +22,31 @@ int main (int argc, char *argv[]) {
             fp = fopen(argv[3], "w");
             fprintf(fp, "\\documentclass{article}\n\n\\begin{document}\n\n<++>\n\n\\end{document}\n");
         } else {
-            printf("-> Invalid or unsupported language `%s'. Run `blr usage' to see supported languages.\n", argv[2]);
+            printf("\033[1;32m->\033[0m Invalid or unsupported language \033[1;32m`%s'\033[0m. Run \033[1;32m`blr usage'\033[0m to see supported languages.\n", argv[2]);
             return 1;
         };
 
         fclose(fp);
-        printf("-> Successfully created %s file `%s'.\n", argv[2], argv[3]);
+        printf("\033[1;32m->\033[0m Successfully created %s file \033[1;32m`%s'\033[0m.\n", argv[2], argv[3]);
         return 0;
     } else if (strcmp(argv[1], "wrexec") == 0 || strcmp(argv[1], "wx") == 0) { 
         if (strcmp(argv[2], "bash") == 0) {
             fp = fopen(argv[3], "w");
             fprintf(fp, "#!/bin/bash\n\n<++>\n");
         } else {
-            printf("-> Cannot create executable type `%s'.", argv[2]);
+            printf("\033[1;32m->\033[0m Cannot create executable type \033[1;32m`%s'\033[0m.", argv[2]);
             return 1;
         };
 
         fclose(fp);
         chmod(argv[3], 0755);
-        printf("-> Successfully created executable %s file `%s'.\n", argv[2], argv[3]);
+        printf("\033[1;32m->\033[0m Successfully created executable %s file \033[1;32m`%s'\033[0m.\n", argv[2], argv[3]);
         return 0;
     } else if (strcmp(argv[1], "usage") == 0) {
         usage();
         return 0;
     } else {
-        printf("-> Invalid command `%s'.\n\n", argv[1]);
+        printf("\033[1;32m->\033[0m Invalid command \033[1;32m`%s'\033[0m.\n\n", argv[1]);
         usage();
         return 1;
     };

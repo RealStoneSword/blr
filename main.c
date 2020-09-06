83e763d30aa43abd8cfa4f720fb38dd0c2cb9b6b
Noah Swerhun <nswerhun@gmail.com>
2020-09-06 14:30:34 -0500

    changed help message and added remove file when filetype is invalid

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #include <sys/stat.h>
 
 void usage() {
-    printf("Usage: blr [-weh] [c,bash,latex,html] [filename]\n\nCommands:\n\t-w\t\twrite a file\n\t-e\t\twrite a executable file\n\t-h\t\tdisplay this help message\n\nLanguages:\n\tc\n\tbash\n\tlatex\n\thtml\nNOTE: language names are case insensitive.\n");
+    printf("Usage: blr [-weh] [language] [filename]\n\nCommands:\n    -w    write a file\n    -e    write an executable file\n    -h    display this help message\n\nLanguages:\n    c\n    bash\n    tex\n    html\nNOTE: language names are case insensitive.\n");
 }
 
-int write(FILE ** pntr, char lang[]);
+int write(FILE ** pntr, char lang[], char name[]);
 
 int main (int argc, char *argv[]) {
 
@@ -18,10 +18,10 @@ int main (int argc, char *argv[]) {
 
     switch (argv[1][1]) {
         case 'w' :
-            write(&fp, argv[2]);
+            write(&fp, argv[2], argv[3]);
             break;
         case 'e' :
-            write(&fp, argv[2]);
+            write(&fp, argv[2], argv[3]);
             chmod(argv[3], 0755);
             break;
         default :
@@ -31,22 +31,22 @@ int main (int argc, char *argv[]) {
 
 
     fclose(fp);
-    printf("Created %s file %s.\n", argv[2], argv[3]);
-    printf("%d\n", argc);
+    printf("Created `%s` file `%s`.\n", argv[2], argv[3]);
     return 0;
 }
 
-int write (FILE ** pntr, char lang[]) {
+int write (FILE ** pntr, char lang[], char name[]) {
     if (strcasecmp(lang, "c") == 0) { 
-        fprintf(*pntr, "#include <stdio.h>\n#include <stdlib.h>\n\nint main() {\n\n    <++>\n\n    return 0;\n}\n");
+        fprintf(*pntr, "#include <stdio.h>\n#include <stdlib.h>\n\nint main(int argc, char *argv[]) {\n\n    <++>\n\n    return 0;\n}\n");
     } else if (strcasecmp(lang, "bash") == 0) {
         fprintf(*pntr, "#!/bin/bash\n\n<++>\n");
-    } else if (strcasecmp(lang, "latex") == 0) {
+    } else if (strcasecmp(lang, "tex") == 0) {
         fprintf(*pntr, "\\documentclass{article}\n\n\\begin{document}\n\n<++>\n\n\\end{document}\n");
     } else if (strcasecmp(lang, "html") == 0) {
         fprintf(*pntr, "<!DOCTYPE HTML>\n\n<html>\n    <head>\n        <++>\n    </head>\n\n    <body>\n        <++>\n    </body>\n</html>");
     } else {
+        remove(name);
         printf("Invalid or unsupported language.\n");
-        return 1;
+        exit(1);
     }
 }
